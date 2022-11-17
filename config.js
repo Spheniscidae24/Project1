68e180d3ccc4625e3bf058161aad98ae9fa3bf4e
Kıvanç Timur <hamitkivanctimur@gmail.com>
2022-11-18 02:05:17 +0300

    Node.js basitwebsite

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "Admin@123",
+        database: "nodedb"
+    }
+}
+
+module.exports = config;
--- index.js
+++ index.js
@@ -2,35 +2,27 @@ const express = require("express");
 
 const app = express();
 
+const userRoutes = require("./routes/users");
+
+
 app.set("view engine","ejs");
+
+
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
+app.use(userRoutes);
+
+
 
 const data = [
-    {id: 1, name: "iphone 14", price: 30000, isActive: true, imageUrl: "1.jpeg" },
-    {id: 2, name: "iphone 15", price: 40000, isActive: false, imageUrl: "2.jpeg" },
-    {id: 3, name: "iphone 16", price: 50000, isActive: true, imageUrl: "3.jpeg" },
+    {id: 1, name: "iphone 12", price: 30000, isActive: true, isHome: true, imageUrl: "1.jpeg" },
+    {id: 2, name: "iphone 15", price: 40000, isActive: false, isHome: false, imageUrl: "2.jpeg" },
+    {id: 3, name: "iphone 16", price: 50000, isActive: true, isHome: true, imageUrl: "3.jpeg" },
 ];
 
-// routes
-
-app.use("/products/:id", function(req, res) {
-    const urun = data.find(u => u.id == req.params.id);
-    res.render("products-details", urun);
-});
-
-app.use("/products", function(req, res) {
-    res.render("products", {
-        urunler: data
-    });
-});
-
-app.use("/", function(req, res) {
-    res.render("index");
-});
 
 
-app.listen(3000, () => {
-    console.log("listenin on port 3000");
+app.listen(5500, () => {
+    console.log("listenin on port 5500");
 });
